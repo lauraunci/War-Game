c8dd40fe2ebf346c822547ca299ae86e850cebf6
Laura Sora <laurasora@Lauras-MacBook-Pro.local>
2019-09-27 00:30:30 -0400

    final commit

--- js/main.js
+++ js/main.js
@@ -1,54 +1,31 @@
 /*----- constants -----*/
-// const cardName = ["A", "r02", "r03", "r04", "r05", "r06", "r07", "r08", "r09", "r10", "J", "Q", "K"]
-const cardName = ["15_A", "2_r02", "3_r03", "4_r04", "5_r05", "6_r06", "7_r07", "8_r08", "9_r09", "10_r10", "12_J", "13_Q", "14_K"]
-
-const suits = ['spades', 'hearts', 'diamonds', 'clubs']
+const cardName = ["15_A", "2_r02", "3_r03", "4_r04", "5_r05", "6_r06", "7_r07", "8_r08", "9_r09", "10_r10", "12_J", "13_Q", "14_K"];
+const suits = ['spades', 'hearts', 'diamonds', 'clubs'];
 const deck = [];
-let playerCard = document.getElementById('player');
-let computerCard = document.getElementById('computer');
+
 
 /*----- app's state (variables) -----*/
 
-let scores, winner, playerDeck, computerDeck;
-playerDeck=[];
-computerDeck=[]
+let scores, winner, playerDeck, computerDeck, gameStarted;
+gameStarted=false;
 
 
 /*----- cached element references -----*/
-
-const pScore = document.querySelector('#score1 div.score');
-const cScore = document.querySelector('#score2 div.score');
-//const btn = document.getElementById('buttonPlay');
+let playerCard = document.getElementById('player');
+let computerCard = document.getElementById('computer');
+const pScore = document.querySelector('#score1 div');
+const cScore = document.querySelector('#score2 div');
+const tscore = document.querySelector('#middle div.display');
+const btnNG = document.getElementById('btnNG');
+const btnPlay = document.getElementById('btnPlay');
 
 /*----- functions -----*/
 
-// function createDeck() {
-//   values.forEach(function(value) {
-//     for(i=0; i<suits.length; i++) {
-//     deck.push(`${value}_${suits[i]}`);
-//     }
-// })
-//   return deck;
-// }
-
 function Card(name, src, value) {
     this.name = name,
-    this.src - src,
+    this.src = src,
     this.value = value
 }
-
-// function createDeck() {
-//     suits.forEach(function (entry) {
-//         cardName.forEach(function(item) {
-//             obj = new Card();
-//             obj.name = `${entry}-${item}`;
-//             obj.src = 'images/'+entry+'/'+obj.name+".svg";
-//             obj.class = `.card.${entry}.${item}`;
-//             deck.push(obj)
-//         })
-//     });
-//     return deck;
-// };
 
 function createDeck() {
     suits.forEach(function (entry) {
@@ -75,62 +52,85 @@ function shuffleDeck() {
 }
 createDeck();
 shuffleDeck();
+
     playerDeck = deck.slice(0, 26);
     computerDeck = deck.slice(26, 52);
 
 /*----- event listeners -----*/
-//document.getElementById('btnNG').addEventListener('click', dealCard);
-document.getElementById('btnPlay').addEventListener('click', play);
+btnNG.addEventListener('click', init);
+btnPlay.addEventListener('click', play);
 
 
 function play() {
     playerCard.src = playerDeck[0].src;
-    playerDeck.shift();
     computerCard.src = computerDeck[0].src;
+    updateScore();
+    render();
+    playerDeck.shift();
     computerDeck.shift();
-    console.log(playerDeck[0]);
-    console.log(computerDeck[0])
+    getWinner();
 }
-console.log(computerDeck)
-//function play() {
-    // let newPlayerDeck = playerDeck;
-    // let newComputerDeck = computerDeck;
-    // let newPlayerCard = newPlayerDeck.slice();
-    // let newComputerCard = newComputerDeck.slice();
-    // playerCard.src = newPlayerCard.src;
-    // computerCard.src = newComputerCard.src;
-    // playerCard.src = playerDeck[1].src;
-    // computerCard.src = computerDeck[1].src;
-//}
 
-const render = function() {
+function render() {
     pScore.textContent = scores.p;
     cScore.textContent = scores.c;
 }
 
-const init = function() {
+function init() {
     scores = {
         p: 0,
         c: 0,
     };
     winner = null;
-
+    gameStarted = true;
+    status();
+    
     render();
 }
 
-init()
+function status() {
+    if(gameStarted === false) {
+        btnPlay.disabled = true; 
+    }
+    else {
+        btnPlay.disabled = false;
+    }
+}
+
+status();
+
+function updateScore(){
+let pcardValue = parseInt(playerDeck[0].value);
+let ccardValue = parseInt(computerDeck[0].value);
+    if(pcardValue > ccardValue) {
+        scores.p++;
+        display.textContent = '';
+    }
+    else if (ccardValue > pcardValue) {
+         scores.c++;
+         display.textContent = '';
+     }
+    else if(ccardValue === pcardValue) {
+         display.textContent = 'War! Hit Play!';
+     }
+}
+
+function getWinner() {
+    if(scores.p === 10) {
+        display.textContent = "Player Won!";
+        gameStarted=false;
+        status();
+    } 
+    else if (scores.c=== 10) { 
+        display.textContent = "Computer Won!";
+        gameStarted=false;
+        status();
+    }
+}
 
 
 
-//dealCard();
 
 
 
 
-
-
-
-
-//
-
-//alert('Hello!');
